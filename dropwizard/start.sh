1d443dbe74670b3ea57f8a7f475dcf2bfc457f43
Ben Oeyen <ben.oeyen@gmail.com>
2016-08-24 18:15:25 +0200

    Added new backend. Added security layer to private backend alls.

--- dropwizard/start.sh
+++ dropwizard/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nginx # start Nginx dispatcher
-java -jar dropwizard-1.8.jar server dropwizard.yml  # start dropwizard backend
+java -jar dropwizard-1.9.jar server dropwizard.yml  # start dropwizard backend
