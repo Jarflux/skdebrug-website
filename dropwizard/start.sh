04594df11ec5e464f4477072e1cecfb1943e51af
Ben Oeyen <ben.oeyen@gmail.com>
2016-10-21 23:54:43 +0200

    new backend.

--- dropwizard/start.sh
+++ dropwizard/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nginx # start Nginx dispatcher
-java -jar dropwizard-1.15.jar server dropwizard.yml  # start dropwizard backend
+java -jar dropwizard-1.16.jar server dropwizard.yml  # start dropwizard backend
