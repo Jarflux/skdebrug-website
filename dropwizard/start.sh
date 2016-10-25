1e1bb761b37cc200b13f7391142030c900fdc231
Ben Oeyen <ben.oeyen@gmail.com>
2016-10-25 14:25:33 +0200

    New backend with improved hotmail security.

--- dropwizard/start.sh
+++ dropwizard/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nginx # start Nginx dispatcher
-java -jar dropwizard-1.22.jar server dropwizard.yml  # start dropwizard backend
+java -jar dropwizard-1.23.jar server dropwizard.yml  # start dropwizard backend
