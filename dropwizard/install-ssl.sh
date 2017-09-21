4d4bfafb99358aaf43b95dafde8853fb9fa349a2
Ben Oeyen <ben.oeyen@gmail.com>
2017-09-21 16:37:15 +0200

    fix script

--- dropwizard/install-ssl.sh
+++ dropwizard/install-ssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nginx -s stop
-certbot --standalone --noninteractive --email ben.oeyen@gmail.com --agree-tos -d www.skdebrug.be -d skdebrug.be --staging
+certbot certonly --standalone --noninteractive --email ben.oeyen@gmail.com --agree-tos -d www.skdebrug.be -d skdebrug.be --staging
 nginx
 (crontab -l ; echo "00 03 1 * * sh /opt/skdebrug/renew-ssl.sh") | crontab -
