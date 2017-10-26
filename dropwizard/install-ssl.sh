10b19fb34e863da75405492e031768b735d55463
Ben Oeyen <ben.oeyen@gmail.com>
2017-10-26 11:59:12 +0200

    New backend JAR, change ssl script, change domain config.

--- dropwizard/install-ssl.sh
+++ dropwizard/install-ssl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 nginx -s stop
-certbot certonly --standalone --noninteractive --email ben.oeyen@gmail.com --agree-tos -d www.skdebrug.be -d skdebrug.be --staging
+certbot certonly --standalone --noninteractive --email ben.oeyen@gmail.com --agree-tos -d www.skdebrug.be
+certbot certonly --standalone --noninteractive --email ben.oeyen@gmail.com --agree-tos -d skdebrug.be
 nginx
 (crontab -l ; echo "00 03 1 * * sh /opt/skdebrug/renew-ssl.sh") | crontab -
