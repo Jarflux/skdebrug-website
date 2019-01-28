2462a987d10e39648e39c99b48784ec081896e15
Ben Oeyen <ben.oeyen@gmail.com>
2019-01-28 17:31:03 +0100

    Added missing files

---
+++ vue/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="app" class="md-layout md-gutter">
+        <div class="md-layout-item md-small-size-100 md-medium-size-50">
+            <NextGame/>
+            <PrevGame/>
+        </div>
+        <div class="md-layout-item md-small-size-100 md-medium-size-50">
+            <Standings/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import VueMaterial from 'vue-material'
+    import 'vue-material/dist/vue-material.min.css'
+    import Standings from './components/Standings.vue'
+    import NextGame from './components/NextGame.vue'
+    import PrevGame from './components/PrevGame.vue'
+
+    Vue.use(VueMaterial);
+
+    export default {
+        name: 'app',
+        components: {
+            Standings, NextGame, PrevGame
+
+        }
+    }
+
+    // import moment from 'moment'
+    //
+    // Vue.filter('formatDate', function(value) {
+    //         if (value) {
+    //             return moment(String(value)).format('MM/DD/YYYY hh:mm')
+    //         }
+    //     }
+    //     <span>{{yourDateString | formatDate}}</span>
+
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ vue/src/components/NextGame.vue
@@ -0,0 +1,42 @@
+<template>
+    <md-card>
+        <div class="panel-heading">
+            <h4>Volgende Wedstrijd</h4>
+        </div>
+        <div class="panel-body">
+            <h4 class="text-center capitalize-first-letter">{{game.date}}</h4>
+            <div class="panel-body-next-match">
+                <div class="in">
+                    <!--<img src="{{game.awayTeam.name}}">-->
+                    <h5>{{game.homeTeam.name}}</h5>
+                </div>
+                <div class="divider">VS</div>
+                <div class="out">
+                    <!--<img src="assets/team/{{game.awayTeam.name}}.png">-->
+                    <h5>{{game.awayTeam.name}}</h5>
+                </div>
+            </div>
+        </div>
+    </md-card>
+</template>
+
+<script>
+    const axios = require('axios');
+
+    export default {
+        data: function () {
+            return {
+                game: null
+            }
+        },
+        mounted() {
+            axios
+                .get('https://api.myjson.com/bins/1dj0rw') // /data/game/next/1/team/1
+                .then(response => (this.game = response.data[0]))
+        }
+    }
+</script>
+
+<style>
+
+</style>
